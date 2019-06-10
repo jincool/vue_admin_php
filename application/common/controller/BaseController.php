bdf3bd96869e3e53161b0f723f042500a7db0c26
jincool <634596685@qq.com>
2019-06-10 10:20:55 +0800

    Update BaseController.php

--- application/common/controller/BaseController.php
+++ application/common/controller/BaseController.php
@@ -8,13 +8,13 @@
 
 class BaseController
 {
-
-    function __construct()
-    {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept,Authorization");
-        header('Access-Control-Allow-Methods: POST,GET');
-    }
+//
+//    function __construct()
+//    {
+//        header('Access-Control-Allow-Origin: *');
+//        header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept,Authorization");
+//        header('Access-Control-Allow-Methods: POST,GET');
+//    }
 
     public static function condition()
     {
