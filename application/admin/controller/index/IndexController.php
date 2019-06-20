b8d95084dc69b24841d3029edb63f7a98287924d
jincool <634596685@qq.com>
2019-06-20 14:15:53 +0800

    Create IndexController.php

---
+++ application/admin/controller/index/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jincool
+ * Date: 2019/3/12
+ * Time: 9:37
+ */
+
+class IndexController extends BaseController
+{
+
+
+    public function indexAction(){
+
+        echo '感谢使用酷框架！';
+    }
+
+    /**
+     * 用户登录获取菜单
+     */
+   public function menuAction(){
+       if (!empty($_POST['username'])){
+           $user=['username'=>$_POST['username'],
+                  'password'=>$_POST['password']
+           ];
+           $data= IndexModel::getUserMenu($user);
+           echo json_encode($data);
+       }
+
+   }
+
+
+}
