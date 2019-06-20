950cf684414a2950533e48ffac32d0c87a143c5d
jincool <634596685@qq.com>
2019-06-20 14:17:17 +0800

    Create config.php
    将配置移到根目录

---
+++ config/config.php
@@ -0,0 +1,26 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: Jincool
+ * Date: 2018/10/8
+ * Time: 14:43
+ */
+return [
+    //应用的整体配置
+    'app' => [
+        'default_platform' => 'admin',//默认模块
+    ],
+    //后台配置
+    'admin' => [
+        'default_file' => 'index',//默认文件夹
+        'default_controller' => 'Index',//默认控制器
+        'default_action' => 'index',//默认方法
+    ],
+    //前台配置
+    'home' => [
+        'default_controller' => '',//默认控制器
+        'default_action' => '',//默认方法
+    ]
+
+
+];
