fe45ea6deb3fe0e0350e956398b3b204ad12e2e0
jincool <634596685@qq.com>
2019-06-20 14:17:35 +0800

    Update Base.php
    支持新建文件夹

--- cool/Base.php
+++ cool/Base.php
@@ -5,7 +5,7 @@
  * 1 读取配置
  * 2 自动加载
  * 3 请求分发
- * User: jincool
+ * User: Jincool
  * Date: 2018/10/8
  * Time: 14:47
  */
@@ -14,21 +14,22 @@ class Base{
     public function run(){
         //加载配置
         $this-> loadConfig();
-        //注销自动加载
-        $this-> registerAutoLoad();
         //获取请求参数
         $this->getRequestParams();
+        //注销自动加载
+        $this-> registerAutoLoad();
         //请求分发
         $this->dispatch();
     }
 //加载配置方法
 private  function  loadConfig(){
         //使用全局变量保存用户配置
-$GLOBALS['config']=require './application/config/config.php';
+$GLOBALS['config']=require './config/config.php';
 
 }
 //创建用户自定义类
 public function userAutoLoad($className){
+
         //定义自定义列表
     $baseClass=[
         'Model'=>'./cool/Model.php',
@@ -42,9 +43,9 @@ public function userAutoLoad($className){
     if(isset($baseClass[$className])){
         require $baseClass[$className];//加载模型类
     }elseif (substr($className,-5)=='Model'){
-        require './application/admin/model/'.$className.'.php';
+        require './application/admin/model/'.FILE.'/'.$className.'.php';
     }elseif (substr($className,-10)=='Controller'){
-        require './application/admin/controller/'.$className.'.php';
+        require './application/admin/controller/'.FILE.'/'.$className.'.php';
     }
 }
 //注册自动加载方法
@@ -57,6 +58,10 @@ private function getRequestParams(){
     $defPlate=$GLOBALS['config']['app']['default_platform'];
     $p=isset($_GET['p']) ? $_GET['p'] : $defPlate;
     define('PLATFORM',$p);
+    //当前文件夹
+    $defFile=$GLOBALS['config'][PLATFORM]['default_file'];
+    $f=isset($_GET['f']) ? $_GET['f'] : $defFile;
+    define('FILE',$f);
     //当前控制器
     $defController=$GLOBALS['config'][PLATFORM]['default_controller'];
     $c=isset($_GET['c']) ? $_GET['c'] : $defController;
