1ddeb58fe3a5c629547a528991d88290da1a65c4
jincool <634596685@qq.com>
2019-06-20 14:17:22 +0800

    Create database.php
    将配置移到根目录

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * 数据库的连接参数配置
+ * User: Jincool
+ * Date: 2018/10/8
+ * Time: 14:58
+ */
+return[
+    //默认数据库的连接参数配置
+    'localhost'=>array(
+        'db'=>'admin',
+        'host'=>'localhost',
+        'user'=>'root',
+        'pwd'=>'123456',
+    ),
+    //自定义数据库的连接参数配置
+    'xxx'=>array(
+        'db'=>'',
+        'host'=>'',
+        'user'=>'',
+        'pwd'=>'',
+    ),
+
+
+];
